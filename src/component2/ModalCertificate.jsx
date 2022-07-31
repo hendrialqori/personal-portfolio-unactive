185ce978c911a5075296b221fce01f6a93b1d568
alqory <teamhendri18@gmail.com>
2022-07-31 14:05:30 +0700

    add padding bottom in modal certificate

--- src/component2/ModalCertificate.jsx
+++ src/component2/ModalCertificate.jsx
@@ -24,7 +24,7 @@ export const Certificate = ({ showModal, showTrigger }) => {
             <ImCross className="text-xl text-white" />
         </button>
         <div
-            className="bg-gray-500 w-11/12 md:w-10/12 lg:w-[768px] mx-auto h-[550px] mb-2 overflow-auto flex flex-wrap gap-y-3 pb-24 md:pb-0" 
+            className="bg-gray-500 w-11/12 md:w-10/12 lg:w-[768px] mx-auto h-[550px] mb-2 overflow-auto flex flex-wrap gap-y-3 pb-24" 
             aria-label="modal-container">
                 <img src={kodio} alt="image" />
                 <img src={js} alt="image" />
