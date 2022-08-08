7ff21d29f7feb9fdf02f37d53c3341868e55b2a3
alqory <teamhendri18@gmail.com>
2022-08-08 13:22:15 +0700

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import { Navigasi } from '../component/Navigasi'
+import { Navigasi as Nav } from '../component/navigasi/Index'
 import { Banner } from '../component/banner'
-import { About } from '../component/about'
+import { About } from '../component/about/Index'
 import { Showcase } from '../component/showcase'
 import { Contact } from '../component/contact'
 import { Footer } from '../component/footer'
@@ -8,7 +8,7 @@ import { Footer } from '../component/footer'
 export default function Home () {
   return (
     <main className='bg-light dark:bg-dark1 font-openSans'>
-        <Navigasi />
+        <Nav />
         <Banner />
         <About />
         <Showcase />
